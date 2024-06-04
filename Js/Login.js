acf8aa2cb2dcdbb8e75cd2fdb9bc8b5708b34fb9
StivenChico <stivenpro55@gmail.com>
2024-06-03 19:38:32 -0500

    users

--- Js/Login.js
+++ Js/Login.js
@@ -1,14 +1,5 @@
-const Verificar=()=>{
-    fetch("/json/Usuarios.json")
-    .then(usuarios=usuarios.json)
-    .then(usuarios=>{
-        usuarios.forEach(usuario => {
-            if(usuario.username==document.getElementById(txtUsuario).value){
-                if(usuario.password==document.getElementById(txtPass).value){
-                    console.log("ingresaste")
-                    location.href("/html/UserHome.html")
-                }
-            }
-        });
-    })
+const verificar=()=>{   
+    fetch("C:/Users/DELL/Documents/ProyectoMantenimiento/json/usuarios.json")
+        .then(res=>res.json())
+        .then(usuario=>console.log(usuario))
 }
