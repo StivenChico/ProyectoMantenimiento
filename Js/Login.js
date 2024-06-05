dadddd750b6d5ea4e94b4cbc0d3b151c09af2889
StivenChico <stivenpro55@gmail.com>
2024-06-04 23:03:01 -0500

    Login Nice
    Ya quedo las comparaciones sencillas de login

--- Js/Login.js
+++ Js/Login.js
@@ -1,5 +1,31 @@
-const verificar=()=>{   
-    fetch("C:/Users/DELL/Documents/ProyectoMantenimiento/json/usuarios.json")
-        .then(res=>res.json())
-        .then(usuario=>console.log(usuario))
+const verificar=()=>{
+     if(document.getElementById("usertxt").value=="" || document.getElementById("passwordtxt").value==""){
+        alert("VERIFICAR CAMPOS VACIOS")
+     }else{
+        IngUser=document.getElementById("usertxt").value
+        IngPass=document.getElementById("passwordtxt").value  
+        IngRol=document.getElementById("rol").value
+        path=""
+        path2=""
+        if(IngRol=="Usuario"){
+            path="/ss/ProyectoMantenimiento/json/Usuarios.json"
+            path2="/ss/ProyectoMantenimiento/html/users.html"
+        }else if(IngRol=="Administrador"){
+            path="/ss/ProyectoMantenimiento/json/Admins.json"
+            path2="/ss/ProyectoMantenimiento/html/admin.html"
+        }
+        fetch(path)
+            .then(res=>res.json())
+            .then(usuario=>{
+                for(i of usuario){
+                    if(i.username==IngUser){
+                        if(i.password==IngPass){
+                            alert("Ingreso Exitoso")
+                            location.href=path2
+                        }
+                    }
+                }
+            })
+    
+    }
 }
