14a1f1948f2695cf075fc5f250993c63f00849f0
StivenChico <stivenpro55@gmail.com>
2024-06-14 21:07:31 -0500

    Update NavBarr
    Correccion json
    verificacion de ingreso debe contener usuario.
    Queda pendiente contenido de Usuario.
    acomodar el nombre del usuario centrarlo de forma vertical
    Dar la bienvenida al usuario(lo ideal es que sea con una ventana y que diga el nombre del usuario)

---
+++ Js/InicioVerify.js
@@ -0,0 +1,11 @@
+const InitVerify=()=>{
+    Usuario=localStorage.getItem("Usuario")
+    if(Usuario==null){
+        alert("Primero debe iniciar sección")
+        location.href="../Index.html"
+    }else{
+        document.getElementById("NombreUser").innerHTML(Usuario.name+" "+ Usuario.surname)
+    }
+}
+
+//InitVerify()
--- Js/Login.js
+++ Js/Login.js
@@ -8,24 +8,20 @@ const verificar = () => {
     IngUser = document.getElementById("usertxt").value;     // recibe los datos en caso no esten vacios
     IngPass = document.getElementById("passwordtxt").value;
     IngRol = document.getElementById("rol").value;
-    path = "";
-    path2 = "";
-    if (IngRol == "Usuario") {
-      path = "/json/Usuarios.json";
-      path2 = "/html/fit.html";               // asigna a que paginas accederá 
-    } else if (IngRol == "Administrador") {     // segun el rol establecido
-      path = "/json/Admins.json";
-      path2 = "/html/admin.html";
-    }
-    fetch(path)
+    path=(IngRol=="Usuario")? "../html/fit_initUser.html":"../html/admin.html";
+    
+    fetch("/json/Usuarios.json")
       .then((res) => res.json())                // establece el formato en el que se recibirá el resultado
       .then((usuario) => {
         for (i of usuario) {                    // iteramos en cada objeto del json 
           if (i.username == IngUser) {
-            if (i.password == IngPass) {        // en caso de tener User y Pass correctos ingresan 
-              alert("Ingreso Exitoso");         // al path que les correspondess
-              localStorage("Usuario", i)
-              location.href = path2;
+            if (i.password == IngPass) {        
+              if(i.Rol==IngRol){         
+                
+                  localStorage.setItem("Usuario", i)
+                  location.href = path;
+                
+              }
             }
           }
         }
