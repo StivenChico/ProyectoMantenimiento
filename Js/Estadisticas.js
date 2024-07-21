670bfe6dcadea0784b95e09c6647da809ac53f45
davicq17 <davidcarrillo1043@gmail.com>
2024-07-20 20:56:19 -0500

    char2

--- Js/Estadisticas.js
+++ Js/Estadisticas.js
@@ -16,3 +16,23 @@ const Init_Graficas=()=>{
     grafica2=document.getElementById("Grafica2");
 
 }
+const renderModelsChart=()=>{
+    axios.get('http://127.0.0.1:3000/regisFisicState'
+    ).then(response =>{
+        const data=response.data; // guardamos el resultado en la variable data
+        const rango=['0-10','11-20','21-30','31-40']
+        const edades= data.map(cliente=>cliente.age); // se procesan las edades
+        const distribucionEdad ={
+            labels:rango,
+            datasets:[{
+                data:edades,// regisro de los datos de la tabla
+                backgroundColor:['blue','red'],//fondo de las grafica en orden
+                borderColor:['rgba(75, 192, 192, 1)'],
+            }]
+        } 
+    
+        new Chart('edadChart',{type:'bar',distribucionEdad})
+    }).catch(err=> console.log('error:', err))
+}
+
+renderModelsChart();
