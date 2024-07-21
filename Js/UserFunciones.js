08ace8b8b841d33305d99d0a9e6303c3efbcabee
StivenChico <stivenpro55@gmail.com>
2024-07-21 15:34:37 -0500

    fixx

--- Js/UserFunciones.js
+++ Js/UserFunciones.js
@@ -5,8 +5,9 @@ const GetRutina=()=>{
         method: 'GET',
         url: 'http://127.0.0.1:3000/GetDiagnostico/'+user.id,
     }).then(function(response){
-        console.log(response.data)
-        if(response.data.lenght>0){
+        //arreglo=response.data
+        //console.log(response.data.length)
+        if(response.data.length>0){
             document.getElementById("RutinaUser").innerHTML=response.data[0].diagnostico
         }else{
             document.getElementById("RutinaUser").innerHTML="Aun no se te ha asignado una rutina"
