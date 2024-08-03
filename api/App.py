be1f56e76a5c17a019dcefbd1c1a5ec4c223d79c
StivenChico <stivenpro55@gmail.com>
2024-08-03 16:27:44 -0500

    api

--- api/App.py
+++ api/App.py
@@ -21,7 +21,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'proyecto_fn'
-mysql = MySQL(app)
+mysql = MySQL(app)"""
 # settings A partir de ese momento Flask utilizará esta clave para poder cifrar la información de la cookie
 app.secret_key = "mysecretkey"
 
